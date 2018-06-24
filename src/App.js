624b0c515c892ba5158a2ea9d27997b77614d4af
Akshit Sehgal <akshitsehgal95@gmail.com>
2018-06-24 13:25:58 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons:[
+      {name:'Tim', age:22},
+      {name:'Kim', age:23},
+      {name: 'Blaze', age:25}
+    ]
+  }
+  clickHandler = (newName) =>{
+    this.setState({
+      persons:[
+        {name:newName, age:22},
+        {name:'Kim', age:23},
+        {name: 'Blaze', age:29}
+      ]
+    });
+  }
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons:[
+        {name:event.target.value, age:22},
+        {name:'Kim', age:23},
+        {name: 'Blaze', age:29}
+      ]
+    });
+  }
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+    return (
+      <div className="App">
+       <h1>Hi, I am React app</h1>
+       <button
+       style={style} 
+       onClick={(event) => this.clickHandler('Martin')}>Switch Name</button>
+       <div>
+       <Person 
+       name={this.state.persons[0].name}
+       age={this.state.persons[0].age} 
+       click={this.clickHandler.bind(this, 'Max')}
+       changed={this.nameChangedHandler}
+       >
+       What`s up?
+       </Person>
+       <Person 
+       name={this.state.persons[1].name}  
+       age={this.state.persons[1].age} 
+
+       />
+       <Person name={this.state.persons[2].name}  age={this.state.persons[2].age} />
+       </div>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1',null,'Does this work?'));
+  }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const person = (props) =>  {
+    return (
+        <div className="Person">
+        <p onClick={props.click}>I am {props.name} and my id is: {props.age}</p>
+        <p>{props.children}</p> 
+        <input type="text" onChange={props.changed} value={props.name}/>   
+        </div>
+    );
+};
+
+export default person;
